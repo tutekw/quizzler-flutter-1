6995eedd6ff7fd3e5401af456f01e627f660f90f
Tomek <tuteq13@gmail.com>
2020-08-14 12:45:17 +0200

    Refactoring of question list to class

--- lib/main.dart
+++ lib/main.dart
@@ -28,32 +28,20 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
 
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
+  List<Question> questionBank = [
+    Question(
+      q: 'You can lead a cow down stairs but not up stairs.',
+      a: false,
+    ),
+    Question(
+      q: 'Approximately one quarter of human bones are in the feet.',
+      a: true,
+    ),
+    Question(
+      q: 'A slug\'s blood is green.',
+      a: true,
+    ),
   ];
-
-  List<bool> anwsers = [
-    false,
-    true,
-    true,
-  ];
-
-  Question q1 = Question(
-    q: 'You can lead a cow down stairs but not up stairs.',
-    a: false,
-  );
-
-  Question q2 = Question(
-    q: 'Approximately one quarter of human bones are in the feet.',
-    a: true,
-  );
-
-  Question q3 = Question(
-    q: 'A slug\'s blood is green.',
-    a: true,
-  );
 
   int questionNumber = 0;
 
@@ -69,7 +57,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -93,7 +81,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnwser = anwsers[questionNumber];
+                bool correctAnwser =
+                    questionBank[questionNumber].questionAnwser;
                 if (correctAnwser == true) {
                   scoreKeeper.add(
                     Icon(
@@ -129,7 +118,8 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnwser = anwsers[questionNumber];
+                bool correctAnwser =
+                    questionBank[questionNumber].questionAnwser;
                 if (correctAnwser == false) {
                   scoreKeeper.add(
                     Icon(
